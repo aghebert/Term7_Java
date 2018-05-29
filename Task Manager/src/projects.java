7759397bcef2a63b5777664a43aa858041b5e05c
aghebert <30908336+aghebert@users.noreply.github.com>
2018-05-29 08:13:36 -0400

    Simple framework

---
+++ Task Manager/src/projects.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class projects extends tasks{
+
+	@Override
+	void orderList(Date dueDate, char priority, Boolean completed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+public tasks() {
+
+}
+
+
+}
---
+++ Task Manager/src/tasks.java
@@ -0,0 +1,5 @@
+import java.util.*;
+
+public abstract class tasks {
+abstract void orderList(Date dueDate, char priority, Boolean completed );
+}
